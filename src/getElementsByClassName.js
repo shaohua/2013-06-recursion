e48f99a923c76e947723021beedb03871063712f
Shaohua Zhou <sh.zhou@gmail.com>
2013-06-04 14:08:04 -0700

    fix getElementsByClassName.js. I miss jQuery.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,29 +6,26 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
-  // var args=Array.prototype.slice.call(arguments);
-  // for (var index in args){
-	 //  console.log(args[index]);
-  // }
-  var that = this;
-  console.log(that);
 
-  var results = document.getElementsByClassName(className);
-  console.log((typeof results), results);
+  //inline function
+  // needs to access a gloal variable matchedNodes
+  function parseDOM(input){
+  	//check for class name of input
+  	if( input.classList.contains(className) ){
+  		matchedNodes.push(input);
+  	}
 
-  console.log('\n');
-  console.log('-----------------------');
-  // console.log(typeof results, results);
-  var tmpParent = document.createElement('div');
-  for(var index=0; index<results.length; index++){
-  	console.log(index, '---', results[index]);
-	tmpParent.appendChild(results[index]);
+  	//loop through all children, NOT childNodes, recursively
+  	for(var index=0; index<input.children.length; index++){
+ 		parseDOM(input.children[index]);
+  	}
+
   }
-  console.log('-----------------------');
 
-  var output = document.createElement("div");
-  output.innerHTML = tmpParent.innerHTML;
-  output = output.firstChild;
-  console.log((typeof output), output);
-  return output;
+  var matchedNodes = [];
+  parseDOM(document.body);
+
+  console.log(matchedNodes);
+
+  return matchedNodes;
 };
